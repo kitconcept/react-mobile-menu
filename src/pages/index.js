391c10e69cd50269e1714a46e9527688f1ae17e1
Timo Stollenwerk <tisto@plone.org>
2018-02-12 09:33:29 +0100

    Add basic reveal animation to front page to make sure it works well with the overlay.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Fade>
+      <h1>Hi people</h1>
+    </Fade>
+    <Zoom>
+      <p>Welcome to your new Gatsby site.</p>
+    </Zoom>
+    <Zoom>
+      <p>Now go build something great.</p>
+    </Zoom>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
